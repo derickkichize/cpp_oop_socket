7bea7194fb80516f545e57692252400b73d325e7
derickkichize <derick.kichize@gmail.com>
2024-05-31 00:07:00 -0300

    Oops

--- share.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-find . -type f \( -name "*.cc" -o -name "*.hh" -o -name "Makefile" -o -name "*.conf" \) \
-	     -exec sh -c 'echo "File: {}" && nl {}' \; | \
-	     xclip -selection c
